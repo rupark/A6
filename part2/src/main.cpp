07b3712d1b865c7268e01c51606de1149ce0735e
rupark <rupar.k@husky.neu.edu>
2020-02-28 17:57:35 -0500

    debug

--- part2/src/main.cpp
+++ part2/src/main.cpp
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <iostream>
 #include "serial.h"
-
+#include <string.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     Status* s = new Status(0, 1, new String("hello"));
     Status* f = new Status(s->serialize()->cstr_);
-    if (s->serialize()->cstr_ == f->serialize()->cstr_) {
+    if (strcmp(s->serialize()->cstr_,f->serialize()->cstr_) == 0) {
         cout << "Status OK" << endl;
     }
 
     Register* r = new Register(0, 1, 8080, new String("127.0.0.1"));
     Register* e = new Register(r->serialize()->cstr_);
-    if (r->serialize()->cstr_ == e->serialize()->cstr_) {
+    if (strcmp(r->serialize()->cstr_,e->serialize()->cstr_) == 0) {
         cout << "Register OK" << endl;
     }
 
     Ack* a = new Ack(0, 1);
     Ack* g = new Ack(a->serialize()->cstr_);
-    if (a->serialize()->cstr_ == g->serialize()->cstr_) {
-        cout << "Register OK" << endl;
+    if (strcmp(a->serialize()->cstr_,g->serialize()->cstr_) == 0) {
+        cout << "Ack OK" << endl;
     }
 
     size_t* ports = new size_t[3];
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[])
     add[2] = new String("127.0.0.3");
     Directory* d = new Directory(0, 1, 3, ports, add);
     Directory* c = new Directory(d->serialize()->cstr_);
-    if (d->serialize()->cstr_ == c->serialize()->cstr_) {
-        cout << "Register OK" << endl;
+    if (strcmp(d->serialize()->cstr_,c->serialize()->cstr_) == 0) {
+        cout << "Directory OK" << endl;
     }
 }
