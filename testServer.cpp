2e97cacde1d6f2359d4fd610a14087386a60aeb0
rupark <rupar.k@husky.neu.edu>
2020-02-27 20:00:06 -0500

    perror

--- testServer.cpp
+++ testServer.cpp
@@ -25,14 +25,14 @@ int main(int argc, char const *argv[])
 //    Node* n1 = new Node(new String("127.0.0.2"),8080, new String("127.0.0.1"), 8080);
 //    cout << "Node Created" << endl;
 //
-//    // DUMPING TABLES
-//    for (size_t j = 0; j < s1->nodes; j++) {
-//        cout << "SERVER: ";
-//        printf("%s\n",s1->addresses[j]->c_str());
-//
+    // DUMPING TABLES
+    for (size_t j = 0; j < s1->nodes; j++) {
+        cout << "SERVER: " << endl;
+        printf("%s\n",s1->addresses[j]->c_str());
+
 //        cout << "NODE: ";
 //        printf("%s\n",n1->addresses[j]->c_str());
-//    }
+    }
 
 
 
