bfd33e13fd17c8051c140d6c2ad000bf1c600da3
rupark <rupar.k@husky.neu.edu>
2020-02-28 17:55:21 -0500

    debug

--- part2/src/main.cpp
+++ part2/src/main.cpp
@@ -7,19 +7,22 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     Status* s = new Status(0, 1, new String("hello"));
-    cout << s->serialize()->cstr_ << endl;
     Status* f = new Status(s->serialize()->cstr_);
-    cout << f->serialize()->cstr_ << endl;
+    if (s->serialize()->cstr_ == f->serialize()->cstr_) {
+        cout << "Status OK" << endl;
+    }
 
     Register* r = new Register(0, 1, 8080, new String("127.0.0.1"));
-    cout << r->serialize()->cstr_ << endl;
     Register* e = new Register(r->serialize()->cstr_);
-    cout << e->serialize()->cstr_ << endl;
+    if (r->serialize()->cstr_ == e->serialize()->cstr_) {
+        cout << "Register OK" << endl;
+    }
 
     Ack* a = new Ack(0, 1);
-    cout << a->serialize()->cstr_ << endl;
     Ack* g = new Ack(a->serialize()->cstr_);
-    cout << g->serialize()->cstr_ << endl;
+    if (a->serialize()->cstr_ == g->serialize()->cstr_) {
+        cout << "Register OK" << endl;
+    }
 
     size_t* ports = new size_t[3];
     ports[0] = 1;
@@ -27,10 +30,11 @@ int main(int argc, char const *argv[])
     ports[2] = 1;
     String** add = new String*[3];
     add[0] = new String("127.0.0.1");
-    add[1] = new String("127.0.0.1");
-    add[2] = new String("127.0.0.1");
+    add[1] = new String("127.0.0.2");
+    add[2] = new String("127.0.0.3");
     Directory* d = new Directory(0, 1, 3, ports, add);
-    cout << d->serialize()->cstr_ << endl;
     Directory* c = new Directory(d->serialize()->cstr_);
-    cout << c->serialize()->cstr_ << endl;
+    if (d->serialize()->cstr_ == c->serialize()->cstr_) {
+        cout << "Register OK" << endl;
+    }
 }
